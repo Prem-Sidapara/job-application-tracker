57993215733a96649754f12643ae5bea9e13b1b2
Prem Sidapara <prem.sidapara.main@gmail.com>
2025-12-28 21:59:50 +0530

    fix jwt decoded id in requireAuth

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,20 +2,24 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const requireAuth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ error: "Not authorized, no token" });
-  }
-
-  const token = authHeader.split(" ")[1];
-
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Not authorized, no token" });
+    }
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded._id).select("_id");
+    const user = await User.findById(decoded.id).select("_id");
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
-    res.status(401).json({ error: "Not authorized" });
+    return res.status(401).json({ error: "Not authorized" });
   }
 };
 
